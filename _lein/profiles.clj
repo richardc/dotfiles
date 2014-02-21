6d55307b2362bb28f2519f286b153255d65b7a30
Richard Clamp <richardc@unixbeard.net>
2014-02-21 21:06:33 +0000

    add quickie - autoruns tests

--- _lein/profiles.clj
+++ _lein/profiles.clj
@@ -2,4 +2,5 @@
  {:dependencies [[clojure-complete "0.2.3"]
                  [org.clojure/tools.namespace "0.2.3"]]
   :injections [(require '(clojure.tools.namespace repl find))]
-  :plugins [[lein-kibit "0.0.8"]]}}
+  :plugins [[lein-kibit "0.0.8"]
+            [quickie "0.2.5"]]}}

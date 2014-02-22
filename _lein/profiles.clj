da9328d84f9456df7f55bdfab266446204fbdf2b
Richard Clamp <richardc@unixbeard.net>
2014-02-22 16:25:33 +0000

    add lein ancient plugin

--- _lein/profiles.clj
+++ _lein/profiles.clj
@@ -3,4 +3,5 @@
                  [org.clojure/tools.namespace "0.2.3"]]
   :injections [(require '(clojure.tools.namespace repl find))]
   :plugins [[lein-kibit "0.0.8"]
-            [quickie "0.2.5"]]}}
+            [quickie "0.2.5"]
+            [lein-ancient "0.5.4"]]}}

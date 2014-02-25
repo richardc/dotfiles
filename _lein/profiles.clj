bbc1f05b503085ac9caa0798f8f2786ff016adec
Richard Clamp <richardc@unixbeard.net>
2014-02-25 02:28:47 +0000

    add lein-try - repl into a random clojar to play

--- _lein/profiles.clj
+++ _lein/profiles.clj
@@ -4,4 +4,5 @@
   :injections [(require '(clojure.tools.namespace repl find))]
   :plugins [[lein-kibit "0.0.8"]
             [quickie "0.2.5"]
-            [lein-ancient "0.5.4"]]}}
+            [lein-ancient "0.5.4"]
+            [lein-try "0.4.1"]]}}

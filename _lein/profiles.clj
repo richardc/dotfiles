bbf4f19c23894fc6b6e857e746e1ad92f59cf016
Richard Clamp <richardc@unixbeard.net>
2014-09-04 22:16:23 +0100

    add cider nrepl middleware to defaults

--- _lein/profiles.clj
+++ _lein/profiles.clj
@@ -4,5 +4,6 @@
   :injections [(require '(clojure.tools.namespace repl find))]
   :plugins [[lein-kibit "0.0.8"]
             [quickie "0.2.5"]
+            [cider/cider-nrepl "0.7.0"]
             [lein-ancient "0.5.4"]
             [lein-try "0.4.1"]]}}

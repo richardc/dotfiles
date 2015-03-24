f1ec18d4972ef2ca456d4bd635398e4d5aa2c452
Richard Clamp <richardc@unixbeard.net>
2015-03-24 16:21:05 +0000

    today we learned about `lein ancient profiles`

--- _lein/profiles.clj
+++ _lein/profiles.clj
@@ -1,9 +1,11 @@
+;; update with: lein ancient profiles
+
 {:user
- {:dependencies [[clojure-complete "0.2.3"]
-                 [org.clojure/tools.namespace "0.2.3"]]
+ {:dependencies [[clojure-complete "0.2.4"]
+                 [org.clojure/tools.namespace "0.2.10"]]
   :injections [(require '(clojure.tools.namespace repl find))]
   :plugins [[lein-kibit "0.0.8"]
-            [quickie "0.2.5"]
-            [cider/cider-nrepl "0.7.0"]
-            [lein-ancient "0.5.4"]
-            [lein-try "0.4.1"]]}}
+            [quickie "0.3.6"]
+            [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+            [lein-ancient "0.6.5"]
+            [lein-try "0.4.3"]]}}

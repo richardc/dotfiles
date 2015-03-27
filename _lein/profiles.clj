7eeea6db1888b2af7d49e708e260b92de4114c04
Richard Clamp <richardc@unixbeard.net>
2015-03-27 02:14:06 +0000

    add lein-exec and spyscope, drop some unused

--- _lein/profiles.clj
+++ _lein/profiles.clj
@@ -2,10 +2,11 @@
 
 {:user
  {:dependencies [[clojure-complete "0.2.4"]
-                 [org.clojure/tools.namespace "0.2.10"]]
-  :injections [(require '(clojure.tools.namespace repl find))]
-  :plugins [[lein-kibit "0.0.8"]
-            [quickie "0.3.6"]
+                 [org.clojure/tools.namespace "0.2.10"]
+                 [spyscope "0.1.5"]]
+  :injections [(require '(clojure.tools.namespace repl find))
+               (require 'spyscope.core)]
+  :plugins [[lein-exec "0.3.4"]
             [cider/cider-nrepl "0.9.0-SNAPSHOT"]
             [lein-ancient "0.6.5"]
             [lein-try "0.4.3"]]}}

0fd7db33d2e6a54f75b7d3f10d4376434fdc0788
Richard Clamp <richardc@unixbeard.net>
2020-04-04 13:41:29 +0100

    Use theme-bobthefish for a fancy prompt
    It handles setting the fish_title too, so delete our copy

---
+++ _config/fish/conf.d/theme-bobthefish.fish
@@ -0,0 +1,12 @@
+# Settings for bobthefish
+#  https://github.com/oh-my-fish/theme-bobthefish
+#
+# No custom fonts
+set -g theme_powerline_fonts no
+
+# Newline cursor
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '$ '
+
+# Color scheme
+set -g theme_color_scheme base16
--- _config/fish/functions/fish_title.fish
+++
@@ -1,4 +0,0 @@
-function fish_title
-    echo (status current-command) ' '
-    prompt_pwd
-end

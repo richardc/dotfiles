64a14b8cd8fdf9d4e6b96dbbfd9a161399e00f88
Richard Clamp <richardc@unixbeard.net>
2024-03-16 00:09:44 +0000

    nvim: Add oil plugin
    A handy way of navigating and adding new files to directories

---
+++ _config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    keys = { { "-", "<CMD>Oil<CR>", desc = "Open parent directory" } },
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+}

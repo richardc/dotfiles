be3fc45607b45394f432cb43ed8289c2d2f21f99
Richard Clamp <richardc@unixbeard.net>
2024-04-02 17:14:14 +0100

    nvim: show colorcolumn for long lines

--- _config/nvim/init.lua
+++ _config/nvim/init.lua
@@ -162,6 +162,10 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Show a hint for long lines
+vim.opt.colorcolumn = '80'
+vim.api.nvim_create_autocmd('Filetype', { pattern = 'rust', command = 'set colorcolumn=100' })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
